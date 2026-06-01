2759f867527f518afaea46632fb65278fb689deb
Bao <nhanhem2ui@gmail.com>
2026-06-01 17:11:17 +0700

    [FIX]: Security

---
+++ src/main/java/com/vastworld/vwbe/common/CorsProperties.java
@@ -0,0 +1,18 @@
+package com.vastworld.vwbe.common;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@ConfigurationProperties(prefix = "app.cors")
+@Component
+@Getter
+@Setter
+public class CorsProperties {
+    private List<String> allowedOrigins;
+    private List<String> allowedMethods;
+    private List<String> allowedHeaders;
+}
--- src/main/java/com/vastworld/vwbe/dto/auth/AuthResponse.java
+++ src/main/java/com/vastworld/vwbe/dto/auth/AuthResponse.java
@@ -1,11 +1,10 @@
 package com.vastworld.vwbe.dto.auth;
 
-import java.util.UUID;
-
 public record AuthResponse(
         String token,
         long expiresIn,
         String username,
-        String email
+        String email,
+        String role
 ) {
 }
